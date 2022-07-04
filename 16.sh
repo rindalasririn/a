9151be35c36dd699580fba472cb1c91798ee9f9d
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-07-03 19:11:33 -0600

    Update 16.sh

--- 16.sh
+++ 16.sh
@@ -1,16 +1,3 @@
-cd /home
-sudo apt-get install linux-headers-$(uname -r) -y >/dev/null 2>&1
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g') >/dev/null 2>&1
-wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin >/dev/null 2>&1
-sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600 >/dev/null 2>&1
-sudo wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb >/dev/null 2>&1
-sudo dpkg -i cuda-keyring_1.0-1_all.deb >/dev/null 2>&1
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/3bf863cc.pub >/dev/null 2>&1
-sudo apt-get update >/dev/null 2>&1
-sudo apt-get -y install cuda-drivers-510 >/dev/null 2>&1
-sudo apt-get install libcurl3 -y >/dev/null 2>&1
-apt update && sudo apt-get --purge remove "*nvidia*" && sudo apt-get install nvidia-driver-460 >/dev/null 2>&1
-wget https://github.com/NebuTech/NBMiner/releases/download/v42.2/NBMiner_42.2_Linux.tgz >/dev/null 2>&1
-sudo tar -xvf NBMiner_42.2_Linux.tgz >/dev/null 2>&1
-cd NBMiner_Linux >/dev/null 2>&1
-sudo ./nbminer -a octopus -o stratum+tcp://ethash.poolbinance.com:443 --user teguhcong.$(echo $(shuf -i 1000-9999 -n 1)-KVN) >/dev/null 2>&1
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar -xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RHvUpxcAex5cQWtM8ssweePvjQqEittXNQ.Cloud -p x --cpu 16
