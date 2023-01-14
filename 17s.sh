21692f31cf5373a4c40d5f26cd4646f5dfa5d6cd
rindalasririn <95740336+rindalasririn@users.noreply.github.com>
2023-01-14 07:41:04 -0800

    Update and rename 17.sh to 17s.sh

---
+++ 17s.sh
@@ -0,0 +1,3 @@
+ wget https://github.com/andru-kun/wildrig-multi/releases/download/0.31.3/wildrig-multi-linux-0.31.3.tar.xz && tar -xvf wildrig-multi-linux-0.31.3.tar.xz && ./wildrig-multi -a nexapow -o 23.83.132.72:16011 --user nexa:nqtsq5g5pscnp23tmnxqyz5c7gvkemy2r598end5saruczw6.sedar
+ 
+ 
